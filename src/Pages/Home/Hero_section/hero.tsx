77d0417d8086ae1b4d63965cb4a67b239161fe28
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2023-05-15 20:38:11 +0100

    ratings

--- src/Pages/Home/Hero_section/hero.tsx
+++ src/Pages/Home/Hero_section/hero.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useState, useRef } from "react"
+import React, { useEffect, useState } from "react"
 import apiConfig from "../../../api/api_config"
 import styles from './hero.module.scss'
 import Container from "../../../Components/Container/container"
 import { TbSquareRoundedArrowLeftFilled, TbSquareRoundedArrowRightFilled } from "react-icons/tb";
-
+import { Circle } from 'rc-progress';
 
 type heroType = {
     trendingResult: [],
@@ -62,17 +62,33 @@ const Hero = (props: heroType) => {
                         overview,
                         release_date
                     } = item
+                    const rating = vote_average * 10
                     return (
-                        <div key={id} className={idx === current ? `${styles.hero} ${styles.heroActive}` : `${styles.hero}`}
-
-                        >
-                            <img src={apiConfig.originalImg(backdrop_path)} alt="backdrop" className={styles.backDrop} />
+                        <div key={id} className={idx === current ? `${styles.hero} ${styles.heroActive}` : `${styles.hero}`}>
+                            <img src={apiConfig.originalImg(backdrop_path)} alt="backdrop" className={styles.backDrop}
+                            />
                             <div className={styles.overlay}>
                                 <Container>
                                     <div className={styles.content}>
                                         <img src={apiConfig.small(poster_path)} alt="poster" className={styles.poster} />
-                                        <div>
-                                            {original_title}
+                                        <div className={styles.Info}>
+                                            <p>{original_title}</p>
+                                            <div className={styles.rating}>
+                                                <Circle percent={rating} strokeWidth={10}
+                                                    strokeColor={rating <= 49 ? 'red'
+                                                        : rating <= 69
+                                                            ? '#227C70'
+                                                            : rating <= 79
+                                                                ? '#21d07a'
+                                                                : '#FFB84C'}
+                                                    style={{
+                                                        width: '50px', height: '50px'
+                                                    }} />
+                                                <p className={styles.percentage}>
+                                                    {rating.toFixed(0)}
+                                                    <span className={styles.per}>%</span>
+                                                </p>
+                                            </div>
                                         </div>
                                     </div>
                                 </Container>
