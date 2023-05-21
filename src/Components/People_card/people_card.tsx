83cee6bc3d9d72ae81d4945167c2f10ddf53a3c1
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2023-05-22 00:40:12 +0100

    custom scroll bar

--- src/Components/People_card/people_card.tsx
+++ src/Components/People_card/people_card.tsx
@@ -1,18 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import requestApi from '../../api/tmdb_api_config'
+import apiConfig from '../../api/api_config'
+import styles from './people_card.module.scss'
 
 
 const PeopleCard = () => {
   const [result, setResult] = useState<[]>([])
 
-  console.log(result)
   useEffect(() => {
     const person = async () => {
       try {
         const { data } = await requestApi.people()
         setResult(data.results)
       } catch (error) {
-        if (error instanceof  Error) {
+        if (error instanceof Error) {
           throw new Error(error.message);
         }
       }
@@ -20,7 +21,24 @@ const PeopleCard = () => {
     person()
   }, [])
   return (
-    <div>PeopleCard</div>
+    <div className={styles.people}>
+      {
+        result.map(itm => {
+          const { name, id, known_for_department, profile_path } = itm
+          return (
+            <div key={id} className={styles.person}>
+              <img src={apiConfig.small(profile_path)} alt="person" className={styles.img} />
+              <div className={styles.overlay}>
+                <div className={styles.content}>
+                  <p>{known_for_department}</p>
+                  <p>{name}</p>
+                </div>
+              </div>
+            </div>
+          )
+        })
+      }
+    </div>
   )
 }
 
