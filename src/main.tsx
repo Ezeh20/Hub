3bc5710683db937f3ae089eddf470e1303a904a6
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2023-05-30 00:00:38 +0100

    scroll to top on mount

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,12 @@ import './index.scss'
 import { ThemeContextProvider } from './Context/theme_context.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { MediaIdProvider } from './Context/current_id_context/current_id.tsx'
+import Scrolltotop from './Scroll-to-top.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
+    <Scrolltotop />
       <ThemeContextProvider>
         <MediaIdProvider>
           <App />
